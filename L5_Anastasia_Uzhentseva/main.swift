8594c3ace07ad8ac61369f145fd86e2de79d91af
Anastasia Uzhentseva <uzhentseva@yahoo.com>
2018-12-05 17:15:53 +0300

    коррекции 3

--- L5_Anastasia_Uzhentseva/main.swift
+++ L5_Anastasia_Uzhentseva/main.swift
@@ -12,18 +12,18 @@ protocol Car {
 }
 
 protocol CarMovable {
-    func move() -> Double
+    func move(newDistance: Double) -> Double
 }
 
 //extension Car {
-//    
+//
 //}
 
 
 
 //создаём класс SportCar согласно протокола Car
 //отличительные свойства turboEngine
-class SportCar : Car {
+class SportCar : Car, CarMovable {
     var time: Double
     var distance: Double
     var turboEngine: turboEngine
@@ -39,8 +39,18 @@ class SportCar : Car {
         self.turboEngine = turboEngine
     }
     
+    func move(newDistance: Double) -> Double {
+        return self.distance + newDistance
+    }
+    
     func calculateSpeed() -> Double {
         return self.distance / self.time
+    }
+}
+
+extension SportCar : CustomStringConvertible {
+    var description: String {
+        return "Спорткар на двигателе \(self.turboEngine.rawValue) проехал \(self.distance) км за \(self.time) часов, скорость составила \(self.calculateSpeed()) км/ч. \n"
     }
 }
 
@@ -50,77 +60,39 @@ class TrunkCar: Car {
     var time: Double
     var distance: Double
     let capacity: Double
-    var currentLoad: Double {
-        didSet {
-            print("\(oldValue)")
-        }
-        willSet {
-            print("\(newValue)")
-        }
-    }
+    var currentLoad: Double
     
-    init?(time: Double, distance: Double, capacity: Double, currentLoad: Double?) {
+    init(time: Double, distance: Double, capacity: Double, currentLoad: Double) {
         self.time = time
         self.distance = distance
         self.capacity = capacity
-        self.currentLoad = currentLoad!
-        if currentLoad! > capacity {
-                return nil
-            }
+        self.currentLoad = currentLoad
     }
     
     func calculateSpeed() -> Double {
         return self.distance / self.time
     }
-
-    func loadTruck (weight: Double?) -> Double {
-        return self.currentLoad + weight!
-    }
-    //Вопрос! где лучше написать условия первышения вместимости?
-}
-
-extension SportCar : CustomStringConvertible {
-    var description: String {
-        return "Спорткар на двигателе \(self.turboEngine.rawValue) проехал \(self.distance) км за \(self.time) часов, скорость составила \(self.calculateSpeed()) км/ч. \n"
+    
+    func loadTruck (weight: Double) -> Double {
+        return self.currentLoad + weight
     }
 }
 
 extension TrunkCar : CustomStringConvertible {
     var description: String {
-        return "Грузовик проехал \(self.distance) км за \(self.time) часов, скорость составила \(self.calculateSpeed()) км/ч. Назрузка составила \(self.currentLoad) из \(self.capacity) возможных.\n"
+        return "Грузовик проехал \(self.distance) км за \(self.time) часов, скорость составила \(self.calculateSpeed()) км/ч. Назрузка составила \(self.loadTruck) из \(self.capacity) возможных.\n"
     }
 }
 
+
+//создаём экземпляры
 let sportCar1 = SportCar(time: 4, distance: 520, turboEngine: .cilinderV)
 let trunkCar2 = TrunkCar(time: 8, distance: 600, capacity: 500, currentLoad: 100)
 
-//загружаем груз в грузовик - здесь не идёт подсчёт в дискрипшн!! разобраться почему
-print(trunkCar2!.loadTruck(weight: 300))
-
-
-// дискрипшн не берет новое значение - Вопрос! Как впихнуть туда newValue?
+// описание экземпляров
 print(sportCar1.description)
-print(trunkCar2!.description)
+print(trunkCar2.description)
 
-
-
-
-
-
-//условия на проверку скорости
-
-//if speed > 0 && speed < 300 {
-//    return "Скорость составила \(speed) км/ч"
-//} else {
-//    return "wasted"
-//}
-//return "неизвестная ошибка"
-//}
-
-//if load > capacity {
-//    return "Нагрузка больше вместимости"
-//} else if weight < 0 {
-//    return "Введено отрицательное значение"
-//} else {
-//    return "Было загружено \(weight) кг груза, общая нагрузка составила \(load) кг"
-//}
+// здесь не идёт подсчёт в дискрипшн!!
+print(sportCar1.move(newDistance: 50))
+print(trunkCar2.loadTruck(weight: 200))
